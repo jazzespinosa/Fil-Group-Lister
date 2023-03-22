b454749c46bfeb7e84410718565cbab711b3ef1a
jazzespinosa <jazel.espinosa96@gmail.com>
2023-03-23 03:17:20 +0800

    Add project files.

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,195 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Fil_Group_Lister
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.labelDirectory = new System.Windows.Forms.Label();
+            this.tboxDirectory = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.treeViewDirectory = new System.Windows.Forms.TreeView();
+            this.labelSecGroups = new System.Windows.Forms.Label();
+            this.cbxFilterSecGroups = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbxInheritance = new System.Windows.Forms.ComboBox();
+            this.labelInheritance = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelDirectory
+            // 
+            this.labelDirectory.AutoSize = true;
+            this.labelDirectory.Location = new System.Drawing.Point(7, 58);
+            this.labelDirectory.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDirectory.Name = "labelDirectory";
+            this.labelDirectory.Size = new System.Drawing.Size(59, 17);
+            this.labelDirectory.TabIndex = 0;
+            this.labelDirectory.Text = "Directory";
+            // 
+            // tboxDirectory
+            // 
+            this.tboxDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tboxDirectory.BackColor = System.Drawing.Color.White;
+            this.tboxDirectory.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tboxDirectory.Location = new System.Drawing.Point(66, 54);
+            this.tboxDirectory.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tboxDirectory.Name = "tboxDirectory";
+            this.tboxDirectory.Size = new System.Drawing.Size(259, 23);
+            this.tboxDirectory.TabIndex = 1;
+            this.tboxDirectory.SizeChanged += tboxDirectory_SizeChanged;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFind.Location = new System.Drawing.Point(331, 52);
+            this.btnFind.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 26);
+            this.btnFind.TabIndex = 2;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += btnFind_Click;
+            // 
+            // treeViewDirectory
+            // 
+            this.treeViewDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeViewDirectory.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.treeViewDirectory.Location = new System.Drawing.Point(7, 84);
+            this.treeViewDirectory.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.treeViewDirectory.Name = "treeViewDirectory";
+            this.treeViewDirectory.Size = new System.Drawing.Size(399, 315);
+            this.treeViewDirectory.TabIndex = 3;
+            this.treeViewDirectory.NodeMouseDoubleClick += treeViewDirectory_NodeMouseDoubleClick;
+            // 
+            // labelSecGroups
+            // 
+            this.labelSecGroups.AutoSize = true;
+            this.labelSecGroups.Location = new System.Drawing.Point(7, 20);
+            this.labelSecGroups.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelSecGroups.Name = "labelSecGroups";
+            this.labelSecGroups.Size = new System.Drawing.Size(74, 17);
+            this.labelSecGroups.TabIndex = 7;
+            this.labelSecGroups.Text = "Sec Groups";
+            // 
+            // cbxFilterSecGroups
+            // 
+            this.cbxFilterSecGroups.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxFilterSecGroups.FormattingEnabled = true;
+            this.cbxFilterSecGroups.Items.AddRange(new object[] {
+            "ALL Groups",
+            "File Groups Only"});
+            this.cbxFilterSecGroups.Location = new System.Drawing.Point(80, 16);
+            this.cbxFilterSecGroups.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cbxFilterSecGroups.Name = "cbxFilterSecGroups";
+            this.cbxFilterSecGroups.Size = new System.Drawing.Size(121, 25);
+            this.cbxFilterSecGroups.TabIndex = 8;
+            this.cbxFilterSecGroups.SelectedIndexChanged += cbxFilterSecGroups_SelectedIndexChanged;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbxInheritance);
+            this.groupBox1.Controls.Add(this.labelInheritance);
+            this.groupBox1.Controls.Add(this.cbxFilterSecGroups);
+            this.groupBox1.Controls.Add(this.labelSecGroups);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(414, 49);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filter";
+            // 
+            // cbxInheritance
+            // 
+            this.cbxInheritance.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxInheritance.FormattingEnabled = true;
+            this.cbxInheritance.Items.AddRange(new object[] {
+            "ALL",
+            "Inherited Only",
+            "Not Inherited Only"});
+            this.cbxInheritance.Location = new System.Drawing.Point(275, 16);
+            this.cbxInheritance.Name = "cbxInheritance";
+            this.cbxInheritance.Size = new System.Drawing.Size(131, 25);
+            this.cbxInheritance.TabIndex = 10;
+            this.cbxInheritance.SelectedIndexChanged += cbxInheritance_SelectedIndexChanged;
+            // 
+            // labelInheritance
+            // 
+            this.labelInheritance.AutoSize = true;
+            this.labelInheritance.Location = new System.Drawing.Point(203, 20);
+            this.labelInheritance.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelInheritance.Name = "labelInheritance";
+            this.labelInheritance.Size = new System.Drawing.Size(72, 17);
+            this.labelInheritance.TabIndex = 9;
+            this.labelInheritance.Text = "Inheritance";
+            // 
+            // MainWindow
+            // 
+            this.AcceptButton = this.btnFind;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(190)))), ((int)(((byte)(190)))));
+            this.ClientSize = new System.Drawing.Size(414, 411);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.treeViewDirectory);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.tboxDirectory);
+            this.Controls.Add(this.labelDirectory);
+            this.Font = new System.Drawing.Font("Roboto", 9.75F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MinimumSize = new System.Drawing.Size(430, 450);
+            this.Name = "MainWindow";
+            this.Text = "Fil Group Lister";
+            this.Load += MainWindow_Load;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDirectory;
+        private System.Windows.Forms.TextBox tboxDirectory;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.TreeView treeViewDirectory;
+        private System.Windows.Forms.Label labelSecGroups;
+        private System.Windows.Forms.ComboBox cbxFilterSecGroups;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbxInheritance;
+        private System.Windows.Forms.Label labelInheritance;
+    }
+}
---
+++ MainWindow.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Windows.Forms;
+
+namespace Fil_Group_Lister
+{
+    public partial class MainWindow : Form
+    {
+        private string currentFindValue { get; set; } = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            cbxFilterSecGroups.Text = "ALL Groups";
+            cbxInheritance.Text = "ALL";
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            string folderPath = tboxDirectory.Text.Trim();
+            if (folderPath.EndsWith("\\")) folderPath = folderPath.Substring(0, folderPath.Length - 1);
+
+            if (folderPath.Length <= 0)
+            {
+                MessageBox.Show("Directory field empty.");
+                tboxDirectory.Clear();
+                return;
+            }
+
+            bool ifDirExists = new DirectoryInfo(tboxDirectory.Text).Exists;
+            if (ifDirExists == false)
+            {
+                MessageBox.Show("Folder path does not exist.");
+                treeViewDirectory.Nodes.Clear();
+                return;
+            }
+
+            currentFindValue = folderPath;
+            createDataTable(folderPath);
+        }
+
+        void createDataTable(string folderPath)
+        {
+            if (folderPath.Length <= 0) return;
+
+            int count = folderPath.Count(x => x == '\\');
+
+            var dt = new DataTable();
+            dt.Columns.Add("FolderName", typeof(string));
+            dt.Columns.Add("Path", typeof(string));
+            dt.Columns.Add("Permission", typeof(string));
+            dt.Columns.Add("AccessType", typeof(string));
+            dt.Columns.Add("ifInherited", typeof(bool));
+            dt.Columns.Add("ifFilGroup", typeof(bool));
+
+            do
+            {
+                string lastFolder = folderPath.Substring(folderPath.LastIndexOf("\\") + 1); //get last folder name
+
+                var dInfo = new DirectoryInfo(folderPath);
+                var dSecurity = dInfo.GetAccessControl(AccessControlSections.Access);
+
+                foreach (FileSystemAccessRule rule in dSecurity.GetAccessRules(true, true, typeof(NTAccount)))
+                {
+                    string permName = rule.IdentityReference.Value;
+                    string permNameFinal = permName;
+                    try
+                    {
+                        SecurityIdentifier securityIdentifier = new SecurityIdentifier(permName);
+                        permNameFinal = securityIdentifier.Translate(typeof(NTAccount)).Value;
+                    }
+                    catch { }
+
+                    string permAccessType = rule.FileSystemRights.ToString();
+                    int countpermAccessType = permAccessType.Count(x => x == ',');
+                    bool permIfInherited = rule.IsInherited ? true : false;
+
+                    if (countpermAccessType > 0)
+                    {
+                        var finalPermAccessType = permAccessType.Substring(permAccessType.LastIndexOf(",") + 1);
+                        permAccessType = permAccessType.Substring(0, permAccessType.Length - (finalPermAccessType.Length + 1));
+                    }
+
+                    permName = permName.Substring(permName.LastIndexOf("\\") + 1);
+
+                    bool ifFilGroup = false;
+
+                    if (permName.Substring(0, 3).ToLower() == "fil")
+                    {
+                        ifFilGroup = true;
+                    }
+                    dt.Rows.Add(lastFolder, folderPath, permName, permAccessType, permIfInherited, ifFilGroup);
+                }
+
+                folderPath = folderPath.Substring(0, folderPath.Length - (lastFolder.Length + 1)); //get next folder path
+
+                count--;
+            } while (count > 0);
+
+            filterDataTable(dt);
+        }
+
+        private void filterDataTable(DataTable dt)
+        {
+            string dtColumnName;
+            bool dtRowValue;
+            DataTable dtInitial = dt.AsEnumerable()
+                .GroupBy(row => new { FolderName = row.Field<string>("FolderName"), Path = row.Field<string>("Path") })
+                .Select(g => g.First())
+                .CopyToDataTable();
+
+            if (cbxFilterSecGroups.Text == "File Groups Only")
+            {
+                dtColumnName = "ifFilGroup";
+                dtRowValue = true;
+
+                if (countDTRows(dt, dtColumnName, dtRowValue) <= 0)
+                {
+                    MessageBox.Show("No groups to display.");
+                    return;
+                }
+
+                DataTable dtFilterSecGroups = dt.AsEnumerable()
+                    .Where(row => row.Field<bool>("ifFilGroup") == true)
+                    .CopyToDataTable();
+
+                dt = dtFilterSecGroups;
+            }
+
+            if (cbxInheritance.Text == "Inherited Only")
+            {
+                dtColumnName = "ifInherited";
+                dtRowValue = true;
+
+                if (countDTRows(dt, dtColumnName, dtRowValue) <= 0)
+                {
+                    MessageBox.Show("No groups to display.");
+                    return;
+                }
+
+                dt = dtIfInherited(dt, true);
+            }
+            else if (cbxInheritance.Text == "Not Inherited Only")
+            {
+                dtColumnName = "ifInherited";
+                dtRowValue = false;
+
+                if (countDTRows(dt, dtColumnName, dtRowValue) <= 0)
+                {
+                    MessageBox.Show("No groups to display.");
+                    return;
+                }
+
+                dt = dtIfInherited(dt, false);
+            }
+            displayTreeView(dt, dtInitial);
+        }
+
+        private DataTable dtIfInherited(DataTable dt, bool ifInherited)
+        {
+            DataTable dataTable = dt.AsEnumerable()
+                    .Where(row => row.Field<bool>("ifInherited") == ifInherited)
+                    .CopyToDataTable();
+
+            return dataTable;
+        }
+
+        private int countDTRows(DataTable dt, string dtColumnName, bool dtRowValue)
+        {
+            return dt.AsEnumerable().Count(row => row.Field<bool>(dtColumnName) == dtRowValue);
+        }
+
+        private int countRows(DataTable dt, string dtColumnName1, string dtRowValue1, string dtColumnName2, string dtRowValue2)
+        {
+            return dt.AsEnumerable().Count(row => row.Field<string>(dtColumnName1) == dtRowValue1
+                                               && row.Field<string>(dtColumnName2) == dtRowValue2);
+        }
+
+        private void displayTreeView(DataTable dt, DataTable dtInitial)
+        {
+            int i = 0;
+            treeViewDirectory.Nodes.Clear();
+
+            while (i < dtInitial.Rows.Count)
+            {
+                DataRow rowInitial = dtInitial.Rows[i];
+
+                string folderName = rowInitial.Field<string>(0);
+                string folderPath = rowInitial.Field<string>(1);
+                string dispFolderName = $"{folderName} ( {folderPath} )";
+
+                TreeNode folderNode = treeViewDirectory.Nodes.Add(dispFolderName);
+                folderNode.NodeFont = new Font(treeViewDirectory.Font.FontFamily, 11, FontStyle.Bold);
+                folderNode.Text = dispFolderName;
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    if (row[0] == folderName && row[1] == folderPath)
+                    {
+                        string folderPermission = row.Field<string>(2);
+                        string accessType = row.Field<string>(3);
+                        string ifInherited = row.Field<bool>(4) ? "Inherited" : "Not Inherited";
+                        if (accessType != null)
+                        {
+                            folderNode.Nodes.Add($"{folderPermission} | ( {accessType}, {ifInherited} )");
+                        }
+                    }
+                }
+                i++;
+            }
+
+            treeViewDirectory.ExpandAll();
+            //treeViewDirectory.Nodes[0].EnsureVisible();
+            treeViewDirectory.SelectedNode = treeViewDirectory.Nodes[0];
+        }
+
+        private void treeViewDirectory_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            TreeNode node = treeViewDirectory.SelectedNode;
+            if (node.Nodes.Count == 0)
+            {
+                string selectedNode = treeViewDirectory.SelectedNode.Text;
+                int index = selectedNode.IndexOf('|');
+                if (index > 0)
+                {
+                    selectedNode = selectedNode.Substring(0, index);
+                    selectedNode = selectedNode.Trim();
+                }
+                Clipboard.SetText(selectedNode);
+            }
+
+        }
+
+        private void cbxFilterSecGroups_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            treeViewDirectory.Nodes.Clear();
+            createDataTable(currentFindValue);
+        }
+
+        private void tboxDirectory_SizeChanged(object sender, EventArgs e)
+        {
+            string tbox = tboxDirectory.Text;
+            tboxDirectory.Clear();
+            tboxDirectory.Text = tbox;
+            tboxDirectory.Focus();
+            tboxDirectory.SelectAll();
+        }
+
+        private void cbxInheritance_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            treeViewDirectory.Nodes.Clear();
+            createDataTable(currentFindValue);
+        }
+
+    }
+}
